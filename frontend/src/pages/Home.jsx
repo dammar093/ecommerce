8451d5a992d9838787f3cf9ff07e369d1cbc0686
dammar093 <dammarrana093@gmail.com>
2024-08-06 20:07:03 +0545

    fetch product data from backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from "../components/Slider"
 import Categories from '../components/Categories'
 import Service from '../components/Service'
 import Products from '../components/Products'
+import axios from 'axios'
+import baseUrl from '../baseUrl'
+import { useDispatch } from 'react-redux'
+import { setProducts } from '../features/productSlice'
 
 
 const Home = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const res = await axios(`${baseUrl}/api/v1/products`, {
+        withCredentials: true
+      });
+      dispatch(setProducts(res.data.data))
+    }
+    fetchProduct()
+  }, [])
   return (
     <main className='mb-[70px]'>
       <section className=''>
