94667ecd4f884d0b05555e65ae8d416287bd15a9
dammar093 <dammarrana093@gmail.com>
2024-08-06 20:05:48 +0545

    add product model

---
+++ backend/src/models/product.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    discountPercentage: {
+      type: Number,  // Corrected this line
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    colors: {
+      type: [String],
+    },
+    sizes: {
+      type: [String],
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: true,
+    },
+    images: {
+      type: [String],  // You can also specify more detailed validation here if needed
+      required: true,
+    },
+    imagesId: {
+      type: [String],
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("product", productSchema);
+
+module.exports = Product;
