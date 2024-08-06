14aa60952c2a8750cc2e68f43025fad2828fd8d2
dammar093 <dammarrana093@gmail.com>
2024-08-06 20:22:26 +0545

    make image size contain

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const CategoryCard = ({ category }) => {
   return (
     <Link to={`/category/category=${category.title}`} className='w-[45vw] h-[170px] md:w-[150px] md:h-[150px] bg-white shadow py-2 flex items-center justify-center flex-col rounded overflow-hidden hover:shadow-xl'>
-      <img className='w-[140px] h-[120px] md:w-[100px] md:h-[100px] object-cover hover:scale-110 transition-all' src={category.image} alt="" />
+      <img className='w-[140px] h-[120px] md:w-[100px] md:h-[100px] object-contain hover:scale-110 transition-all' src={category.image} alt="" />
       <h2 className='text-md text-gray-700 font-medium uppercase'>{category.title}</h2>
     </Link>
   )
