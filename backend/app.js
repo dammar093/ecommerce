fc69ab00a12b559a77368b0e0c7afaab0ebb747d
dammar093 <dammarrana093@gmail.com>
2024-08-26 12:16:16 +0545

    fix env

--- backend/app.js
+++ backend/app.js
@@ -7,26 +7,27 @@ const orderRouter = require("./src/routes/order")
 const reviewRouter = require("./src/routes/review")
 const cors = require("cors")
 const app = express();
-const cookieParser = require("cookie-parser")
-const path = require("path");
+const cookieParser = require("cookie-parser");
+const constants = require("./src/config/config");
+
 
 //middelwares
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: constants.CORS_ORIGIN,
     credentials: true
 }))
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 // app.use(express.static(path.resolve(__dirname, 'public/dist')));
 
 //routes
 //user routes
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/categories",categoryRouter)
-app.use("/api/v1/products",productRouter)
-app.use("/api/v1/carts",cartRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/categories", categoryRouter)
+app.use("/api/v1/products", productRouter)
+app.use("/api/v1/carts", cartRouter)
 app.use("/api/v1/orders", orderRouter);
-app.use("/api/v1/reviews",reviewRouter);
+app.use("/api/v1/reviews", reviewRouter);
 module.exports = app;
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,17 +1,17 @@
 const dotenv = require("dotenv").config();
 
-const constants ={
-  port:process.env.PORT || 8000,
-  databaseName:"hamro-mart",
-  CORS_ORIGIN:process.env.CORS_ORIGIN,
-  dbUrl:process.env.MONGO_URL,
-  cloudinaryCloudName : process.env.CLOUDINARY_CLOUD_NAME,
-  cloudinaryApiKey : process.env.CLOUDINARY_API_KEY,
-  coudinaryApiSecretKey : process.env.CLOUDINARY_API_SECRET,
-  jwtSecret:process.env.JWT_SECRET,
-  eSewaUrl:process.env.ESEWA_URL,
-  eSewaProductCode:process.env.ESEWA_PRODUCT_CODE,
-  eSewaSecretKey:process.env.ESEWA_SECRET_KEY
+const constants = {
+  port: process.env.PORT || 8000,
+  databaseName: "hamro-mart",
+  CORS_ORIGIN: process.env.CORS_ORIGIN,
+  dbUrl: process.env.MONGO_URL,
+  cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  coudinaryApiSecretKey: process.env.CLOUDINARY_API_SECRET,
+  jwtSecret: process.env.JWT_SECRET,
+  eSewaUrl: process.env.ESEWA_URL,
+  eSewaProductCode: process.env.ESEWA_PRODUCT_CODE,
+  eSewaSecretKey: process.env.ESEWA_SECRET_KEY
 }
 
 module.exports = constants
