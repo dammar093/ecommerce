17e160a878196a91dff254955380b49ddd5e7c9d
dammar093 <dammarrana093@gmail.com>
2024-08-26 09:23:28 +0545

    add empty cart

--- frontend/src/components/EmptyCart.jsx
+++ frontend/src/components/EmptyCart.jsx
@@ -3,16 +3,12 @@ import { Link } from 'react-router-dom'
 
 const EmptyCart = () => {
   return (
-    <section className='w-full  object-fit flex justify-center items-center'>
+    <section className='w-full  h-[80vh] flex justify-center items-center'>
       <div className='w-full mt-8'>
-        <h2 className='text-xl md:text-2xl font-semibold text-gray-600 uppercase text-center'>Your cart is empty</h2>
-        <div className='w-full'>
-          <div className='w-full flex justify-center '>
-            <video className='md:w-[300px] w-full h-auto' loading="lazy" muted="muted" src="https://cdnl.iconscout.com/lottie/premium/thumb/empty-box-12088211-9861805.mp4" type="video/mp4" autoPlay="autoplay" loop="loop"></video>
-          </div>
-          <br />
+        <h2 className=' animate-pulse text-3xl font-semibold text-gray-600 uppercase text-center'>Your cart is empty</h2>
+        <div className='w-full mt-4'>
           <div className='w-full flex justify-center'>
-            <Link className='text-md uppercase  text-white  bg-[#AE56EF]  rounded-full py-2 px-4 hover:bg-[#682597] transition-all delay-100' to="/products">
+            <Link className='text-md uppercase  text-white  bg-[#AE56EF]  rounded-full py-2 px-8 hover:bg-[#682597] transition-all delay-100' to="/products">
               continue shopping
             </Link>
           </div>
