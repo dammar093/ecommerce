98be18de8eaf083638ebab99e2531afe167d7778
dammar093 <dammarrana093@gmail.com>
2024-08-31 05:56:18 +0545

    add make analytical dynamically

--- frontend/src/components/admin/AnalyticalLine.jsx
+++ frontend/src/components/admin/AnalyticalLine.jsx
@@ -1,28 +1,26 @@
 import { Stack, useMediaQuery } from '@mui/material';
 import { LineChart } from '@mui/x-charts/LineChart';
-import { useEffect, useState } from 'react';
-const sellData = [4000, 3000, 2000, 2780, 1890, 2390, 3490, 9000, 3008, 4200, 3500, 4500];
-const incomeData = [2400, 1398, 9800, 3908, 4800, 3800, 4300, 9800, 3908, 4800, 3800, 5000];
-const xLabels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
-];
+import { useSelector } from 'react-redux';
 
 
 function AnalyticalLine() {
+  const { totalSell, totalDeliverd } = useSelector(state => state.count)
   const isSmallScreen = useMediaQuery('(max-width:600px)');
   const isMediumScreen = useMediaQuery('(min-width:1024px) and (max-width:1024px)');
   const isLargeScreen = useMediaQuery('(min-width:960px)');
+  const sellData = [0, 0, 0, 0, 0, 0, 0, 0, totalDeliverd];
+  const incomeData = [0, 0, 0, 0, 0, 0, 0, 0, totalSell];
+  const xLabels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+  ];
 
   const getChartDimensions = () => {
     if (isSmallScreen) {
@@ -42,7 +40,7 @@ function AnalyticalLine() {
         width={width}
         height={300}
         series={[
-          { data: sellData, label: 'Sell' },
+          { data: sellData, label: 'Placed Orders' },
           { data: incomeData, label: 'Income' },
         ]}
         xAxis={[{ scaleType: 'point', data: xLabels }]}
